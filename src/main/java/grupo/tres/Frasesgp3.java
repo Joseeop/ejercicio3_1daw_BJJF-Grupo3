b988c90785f8472c90a797dfd7e5f21414773bfe
Joseeop <92110063+Joseeop@users.noreply.github.com>
2021-10-19 16:36:25 +0200

    PrimeraFrase
    Empezamos con las frases

---
+++ src/main/java/grupo/tres/Frasesgp3.java
@@ -0,0 +1,11 @@
+package grupo.tres;
+
+public class Frasesgp3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(Empezamos esta bonita historia);
+	}
+
+}

07931c4465436df74a3a76487518ec41be68ed4e
francisMLG2014 <francisMLG2014@gmail.com>
2021-10-19 16:52:31 +0200

    frase 2
    que fastidio :3

--- src/main/java/grupo/tres/Frasesgp3.java
+++ src/main/java/grupo/tres/Frasesgp3.java
@@ -10,6 +10,7 @@ public class Frasesgp3 {
 		System.out.println(pero bueno, que le vamos a hacer, en verdad mola);
 		System.out.println(Su hijo sabe hacer esto y nosotros no);
 		System.out.println(No mola nada loco!);
+		System.out.println(siento como que nos tenemos que ir fastidiando entre nosotros xd);
 	}
 
 }

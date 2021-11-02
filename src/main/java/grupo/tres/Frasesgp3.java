99b730b2d23c230d3013eaea7c493feaa1774a1e
Joseeop <92110063+Joseeop@users.noreply.github.com>
2021-11-02 15:41:47 +0100

    Problemas solucionados 0211

--- src/main/java/grupo/tres/Frasesgp3.java
+++ src/main/java/grupo/tres/Frasesgp3.java
@@ -5,16 +5,16 @@ public class Frasesgp3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println(Empezamos esta bonita historia);
-		System.out.println(Si no fuese por Miguel no la habr�amos empezado);
-		System.out.println(pero bueno, que le vamos a hacer, en verdad mola);
-		System.out.println(Su hijo sabe hacer esto y nosotros no);
-		System.out.println(No mola nada loco!);
-		System.out.println(siento como que nos tenemos que ir fastidiando entre nosotros xd);
-		System.out.println(vaya problem�n que tenemos con los conflictos eh chicos...);
-		System.out.println(Berta nos realentiza en exceso);
-		System.out.println(aunque se esfuerza en llevarlo adelante chicos);
-		System.out.println(Berta nos realentiza en exceso);
+		System.out.println("Empezamos esta bonita historia");
+		System.out.println("Si no fuese por Miguel no la habr�amos empezado");
+		System.out.println("pero bueno, que le vamos a hacer, en verdad mola");
+		System.out.println("Su hijo sabe hacer esto y nosotros no");
+		System.out.println("No mola nada loco!");
+		System.out.println("siento como que nos tenemos que ir fastidiando entre nosotros xd");
+		System.out.println("vaya problem�n que tenemos con los conflictos eh chicos...");
+		System.out.println("Berta nos realentiza en exceso");
+		System.out.println("aunque se esfuerza en llevarlo adelante chicos");
+		System.out.println("Berta nos realentiza en exceso");
 	}
 	
 

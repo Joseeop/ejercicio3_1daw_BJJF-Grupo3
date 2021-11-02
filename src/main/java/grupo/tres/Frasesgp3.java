c02c6f70f95dec60457a991498ef8c7bc3149c46
francisMLG2014 <francisMLG2014@gmail.com>
2021-11-02 17:15:00 +0100

    final?
    final final

--- src/main/java/grupo/tres/Frasesgp3.java
+++ src/main/java/grupo/tres/Frasesgp3.java
@@ -4,41 +4,7 @@ public class Frasesgp3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		System.out.println("Empezamos esta bonita historia");
-		System.out.println("Si no fuese por Miguel no la habriamos empezado");
-		System.out.println("pero bueno, que le vamos a hacer, en verdad mola");
-		System.out.println("madre mia");
-		System.out.println("Suerte con la clases online");
-		System.out.println("Empezamos esta bonita historia");
-		System.out.println("Si no fuese por Miguel no la habríamos empezado");
-		System.out.println("pero bueno, que le vamos a hacer, en verdad mola");
-		System.out.println("Su hijo sabe hacer esto y nosotros no");
-		System.out.println("No mola nada loco!");
-		System.out.println("siento como que nos tenemos que ir fastidiando entre nosotros xd");
-		System.out.println("vaya problemón que tenemos con los conflictos eh chicos...");
-		System.out.println("aunque se esfuerza en llevarlo adelante chicos");
-		System.out.println("ahora miguel le esta explicando");
-		System.out.println("Debeis veros peaky blinders");
-		System.out.println("ahora miguel le está explicando");
-		System.out.println("Debeis veros peaky blinders");
-		System.out.println("by the order of the fooking peaky blindeeers");
-		System.out.println("Por ahora prefiero esperarme a la semana que viene al estreno de arcane");
-		System.out.println("estoy deseando que salga la temporada 6 la vd");
-		System.out.println("¿Cómo seguimos esta historia?");
 
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
 		System.out.println("Hola id escribiendo si quereis alguna frase :D");
 		System.out.println("Erase una vez una mujer llamada encarni...");
 		System.out.println("Encarni era mi tia");

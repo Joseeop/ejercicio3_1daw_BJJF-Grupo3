64620e255cc96f20e409def603badeaa9132af8b
francisMLG2014 <francisMLG2014@gmail.com>
2021-11-02 17:12:10 +0100

    comilon
    comida

--- src/main/java/grupo/tres/Frasesgp3.java
+++ src/main/java/grupo/tres/Frasesgp3.java
@@ -48,15 +48,16 @@ public class Frasesgp3 {
 		System.out.println("Y no es precisamente de su rollo");
 		System.out.println("Por lo que se puso a mirar hasta encontrar la casa de Bernarda Alba");
 		System.out.println("a mi tia solo le interesa hacer ganchillo");
-		System.out.printl("es muy agil con los dedos")
+		System.out.printl("es muy agil con los dedos");
 		System.out.println("Y jugar a la petanca con su perro");
 		System.out.println("puede resultar muy util tener personas en la familia que practiquen el ganchillo ,eh ");
-		System.out.println("claro, te pueden hacer jerseys")
+		System.out.println("claro, te pueden hacer jerseys");
 		System.out.println("antes jugaba mas a la petanca pero su perro murio y ha dejado de jugar");
 		System.out.println("ahora solo se encierra en casa a acariciar a su gato misifus y comer galletas de mantequilla");
-		System.out.println("lo paso muy mal, y como la petanca le recordaba a pepe pues lo dejo")
-		System.out.println("pepe era su perro, un galgo ")
-		System.out.println("un galgo que en vez de correr saltaba ")
+		System.out.println("lo paso muy mal, y como la petanca le recordaba a pepe pues lo dejo");
+		System.out.println("pepe era su perro, un galgo ");
+		System.out.println("un galgo que en vez de correr saltaba ");
+		System.out.println("y que comia mas que yo en las cenas de navidad");
 		
 		
 		

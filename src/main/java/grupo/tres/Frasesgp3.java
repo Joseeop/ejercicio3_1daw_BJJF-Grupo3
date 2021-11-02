10e3ca6a1317f4057e5aaf2dca1ff53766db4206
francisMLG2014 <francisMLG2014@gmail.com>
2021-11-02 15:58:20 +0100

    frasecita
    ojo bro

--- src/main/java/grupo/tres/Frasesgp3.java
+++ src/main/java/grupo/tres/Frasesgp3.java
@@ -22,6 +22,7 @@ public class Frasesgp3 {
 		System.out.println("by the order of the fooking peaky blindeeers");
 		System.out.println("Por ahora prefiero esperarme a la semana que viene al estreno de arcane");
 		System.out.println("estoy deseando que salga la temporada 6 la vd");
+
 	}
 	
 
